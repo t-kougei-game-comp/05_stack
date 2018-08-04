f806886d972252b30b12a616c5344f6791a7883f
Takashi Imagire <imagire@gmail.com>
2018-08-04 11:28:19 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct stack{
+    enum{
+        BUF_SIZE = 10,
+    };
+    int top;
+    int buf[BUF_SIZE];
+};
+// スタックの初期化
+void statc_initialize(stack *p){
+    p->top = 0;
+}
+// スタックに値を追加(失敗したらfalse)
+bool stack_push(stack *p, int n){
+    if(stack::BUF_SIZE <= p->top) return false;
+    p->buf[p->top++] = n;
+    return true;
+}
+// スタックから取り出す（-1はデータがなかった場合）
+int stack_pop(stack *p){
+    if(p->top <= 0) return false;
+    return p->buf[--p->top];
+}
+// スタックの上から数えたデータを参照する（-1はデータがなかった場合）
+int stack_get(stack *p, int idx){
+    int i = p->top - idx - 1;
+    if(i < 0 || stack::BUF_SIZE <= i) return -1;
+    return p->buf[i];
+}
+
 
 int main(int argc, char *argv[]) 
 {
     char str[7];
     
+    stack s;
+    statc_initialize(&s);
+    
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        int n = atoi(str);
+        switch(n){
+        default:
+            // スタックに追加
+            stack_push(&s ,n);
+            break;
+        case -1:
+            // スタックから取り出す
+            int v = stack_pop(&s);
+            if(v != -1) printf("%d", n);
+            printf("\n");
+            break;
+        case 0:
+            // スタックを表示
+            for(int i = 0; i < stack::BUF_SIZE; i++){
+                int v = stack_get(&s, i);
+                if(v == -1) break;
+                if(i != -1) printf(",");
+                printf("%d", n);
+            }
+            printf("\n");
+            break;
+        }
     }
 
     return 0;
